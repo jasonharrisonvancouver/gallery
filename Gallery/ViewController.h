f90f9b8893a88296bfbb843cca3a6962d793389c
Jason Harrison <jasonharrisonvancouver@gmail.com>
2019-01-20 22:45:43 -0800

    Initial Commit

---
+++ Gallery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gallery
+//
+//  Created by jason harrison on 2019-01-20.
+//  Copyright © 2019 jason harrison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GalleryTests/GalleryTests.m
@@ -0,0 +1,37 @@
+//
+//  GalleryTests.m
+//  GalleryTests
+//
+//  Created by jason harrison on 2019-01-20.
+//  Copyright © 2019 jason harrison. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GalleryTests : XCTestCase
+
+@end
+
+@implementation GalleryTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
